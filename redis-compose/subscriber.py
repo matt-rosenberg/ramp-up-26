7eba3edcd9b655ad6cfeccf1eaf85d29997fa1d9
Matt Rosenberg <rosenbergm1986@gmail.com>
2026-06-08 13:12:17 -0400

    put everything in redis-compose

---
+++ redis-compose/subscriber.py
@@ -0,0 +1,12 @@
+import redis
+
+r = redis.Redis(host="redis", port=6379, decode_responses=True)
+
+sub = r.pubsub()
+sub.subscribe("MyChannel")
+
+print("Waiting for messages: ")
+
+for message in sub.listen():
+    if message["type"] == "message":
+        print("Received message: ", message["data"])
